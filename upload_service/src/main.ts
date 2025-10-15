2ce33e629f71949b6f13eae8308f0e03be937273
dyan <luvteam.dyan@gmail.com>
2025-10-15 11:01:44 +0700

    change upload

--- upload_service/src/main.ts
+++ upload_service/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT || 3002;
 
-
+  console.log(`PORT: ${port}`);
   // Create microservice application
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
