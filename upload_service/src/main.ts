a14065b7c15fa11e3ba79473d4eaa7316ea7a50d
dyan <luvteam.dyan@gmail.com>
2025-10-16 09:08:59 +0700

    test new workflow v2

--- upload_service/src/main.ts
+++ upload_service/src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   console.log(`PORT: ${port}`);
   console.log(`PORT: ${port}`);
   console.log(`PORT: ${port}`);
+  console.log(`PORT: ${port}`);
   // Create microservice application
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
