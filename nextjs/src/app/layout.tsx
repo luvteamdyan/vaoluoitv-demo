80dd0337ef6ff40b0d6bed9176b0d12259f8dd94
dyan <luvteam.dyan@gmail.com>
2025-11-03 14:46:45 +0700

    test build cache, cleanup resource

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -14,9 +14,6 @@ const geistSans = Geist({
   subsets: ["latin"],
   display: 'swap',
 });
-console.log('Geist Sans font loaded:', geistSans);
-console.log('Geist Sans font loaded:', geistSans);
-console.log('Geist Sans font loaded:', geistSans);
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
